f68eb3da1dafa414f4dda630a7f2f41e277fcb22
vrza93 <137451859+vrza93@users.noreply.github.com>
2023-08-26 18:26:00 +0200

    Update vrza.js

--- vrza.js
+++ vrza.js
@@ -1,8 +1,8 @@
 const randomNumber1 = Math.ceil(Math.random() * 6);
 const randomNumber2 = Math.ceil(Math.random() * 6);
 
-document.querySelector(".img1").setAttribute("src", "/Dice Game/images/dice" + randomNumber1 + ".png");
-document.querySelector(".img2").setAttribute("src", "/Dice Game/images/dice" + randomNumber2 + ".png");
+document.querySelector(".img1").setAttribute("src", "images/dice" + randomNumber1 + ".png");
+document.querySelector(".img2").setAttribute("src", "images/dice" + randomNumber2 + ".png");
 
 if (randomNumber1 > randomNumber2) {
     document.querySelector("h1").textContent="Player 1 WON !";
